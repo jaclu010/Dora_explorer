e8c692256e727bdb6fc989df5fd09f275fde201a
marlu819 <marlu819@student.liu.se>
2016-11-17 10:43:39 +0000

    Refactored sensor code.

--- Dora/steering.py
+++ Dora/steering.py
@@ -1,80 +1,69 @@
 import serial
 import threading
-from global_vars import *
+import global_vars as gv
 import os
-#from main import find_usb
+import common
 
 
-serUSB1 = None
-global movement_dir
-def find_usb(port_id):
-	port = ""
-        for file in os.listdir("/dev/serial/by-id"):
-		if port_id in file:
-			symlink = os.path.join("/dev/serial/by-id/", file)
-			port = str(os.path.realpath(symlink))
-	return port
+gv.serUSB1 = None
 
 try:
-    print("Open port plx")
-    serUSB1 = serial.Serial(find_usb(STEERING_PORT), writeTimeout=0)
-    serUSB1.baudrate = BAUDRATE_STEERING
+    gv.serUSB1 = serial.Serial(common.find_usb(gv.STEERING_PORT), writeTimeout=0)
+    gv.serUSB1.baudrate = gv.BAUDRATE_STEERING
 except serial.serialutil.SerialException, exc:
     print("USB1 IT NO WORK, Fixa saken Gomba %s" % exc)
-
+    
+    
     
 def steering_serial(tid):
     """
     Send commands to the steering module
     """
-    print("Thread" + str(tid) + "main loop initialized")
+    print("Thread" + str(tid) + " main loop initialized")
 
-    global serUSB1, running, condition
-    global movement_dir
     prev_movement = "none"
-    print(serUSB1.isOpen())
-    while running:
-        print(movement_dir)
-        if (serUSB1 != None):
+    print(gv.serUSB1.isOpen())
+    while gv.running:
+        if (gv.serUSB1 != None):
             try:
-                if (serUSB1.isOpen()):
-                    condition.acquire()
+                if (gv.serUSB1.isOpen()):
+                    gv.condition.acquire()
                     while True:
-                        temp_move = movement_dir
-                        if movement_dir != "none":
-                            #print(movement_dir)    
+                        temp_move = gv.movement_dir
+                        if gv.movement_dir != "none":
                             break
-                        condition.wait()
-                    condition.release()
-                    #print(movement_dir)
+                        gv.condition.wait()
+                    gv.condition.release()
+                    #print(temp_move)
                     if temp_move == "forward" and prev_movement != "forward":
-                        print("here")
-                        serUSB1.write("f".encode())
-                        serUSB1.write("s".encode())
-                        serUSB1.write(chr(100))
+                        gv.serUSB1.write("f".encode())
+                        gv.serUSB1.write("s".encode())
+                        gv.serUSB1.write(chr(100))
                         prev_movement = "forward"
-                    elif movement_dir == "backward" and prev_movement != "backward":
-                        serUSB1.write("b".encode())
-                        serUSB1.write("s".encode())
-                        serUSB1.write(chr(100))
+                        
+                    elif temp_move == "backward" and prev_movement != "backward":
+                        gv.serUSB1.write("b".encode())
+                        gv.serUSB1.write("s".encode())
+                        gv.serUSB1.write(chr(100))
                         prev_movement = "backward"
-
-                    elif movement_dir == "left" and prev_movement != "left":
-                        serUSB1.write("l".encode())
-                        serUSB1.write("s".encode())
-                        serUSB1.write(chr(100))
+                        
+                    elif temp_move == "left" and prev_movement != "left":
+                        gv.serUSB1.write("l".encode())
+                        gv.serUSB1.write("s".encode())
+                        gv.serUSB1.write(chr(100))
                         prev_movement = "left"
 
-                    elif movement_dir == "right" and prev_movement != "right":
-                        serUSB1.write("l".encode())
-                        serUSB1.write("s".encode())
-                        serUSB1.write(chr(100))
+                    elif temp_move == "right" and prev_movement != "right":
+                        gv.serUSB1.write("r".encode())
+                        gv.serUSB1.write("s".encode())
+                        gv.serUSB1.write(chr(100))
                         prev_movement = "right"
                             
-                    elif movement_dir == "stop" and prev_movement != "stop":
-                        serUSB1.write("s".encode())
-                        serUSB1.write(chr(0))
+                    elif temp_move == "stop" and prev_movement != "stop":
+                        gv.serUSB1.write("s".encode())
+                        gv.serUSB1.write(chr(0))
                         prev_movement = "stop"
 
             except serial.serialutil.SerialException, exc:
                 print("An error occured with the serial connection on the steering port: %s" % exc)
+                raise serial.serialutil.SerialException

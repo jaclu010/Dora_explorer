1e27820b82044744703930dafcbb7659b5bf5296
Johan Nilsson <johni198@student.liu.se>
2016-11-21 19:47:48 +0100

    This program generates a new txt-file containing only "laser-data" from the robot_log

---
+++ SLAMTest/fixTxt.py
@@ -0,0 +1,13 @@
+with open("robotLog.txt", "r") as f:
+	a = f.readlines()
+newlist = []
+for e in a:
+	if ("[laser]" in e):
+		index = e.find("[l")
+		newlist.append(e[index+len("[laser]"):])
+
+with open("slamfixed2.txt", "w") as f2:
+	for e in newlist:
+		f2.write(e)
+
+

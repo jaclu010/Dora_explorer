1d6025c70428ffbbd8eb1f50be6bb6a67d8294d5
Johan Nilsson <johni198@student.liu.se>
2016-11-29 13:26:45 +0100

    Update slamclass.py

--- SLAMTest/slamclass.py
+++ SLAMTest/slamclass.py
@@ -53,6 +53,9 @@ class Laser_Data:
         self.laser_array = []
         self.lines = []
         self.lines_score = []
+
+        self.cornor = []
+
         self.goodr = []
         self.vector_x = []
         self.vector_y = []
@@ -64,6 +67,35 @@ class Laser_Data:
         self.laser_array = l_array
         self.fixval = self.get_fix_value()
         self.good_read()
+
+    def set_cornor(self):
+        self.cornor = []
+        for i in range(len(self.lines_score)):
+            l1 = self.lines_score[i][0]
+            for j in range(len(self.lines_score)):
+                l2 = self.lines_score[j][0]
+
+                p1 = l1[0]
+                p2 = l1[1]
+                p3 = l2[0]
+                p4 = l2[1]
+
+                #s1,s2,s3 are a binary statement, containing info if two lines is built by same point
+                s1 = (i != j)
+                s2 = (p1 == p3) or (p2 == p4)
+                if(s1 and s2):
+                    self.cornor.append((i,j))
+
+                #s2 = (l1[1][0] == l2[0][0] and l1[1][1] == l2[0][1]) or (l1[0][0] == l2[1][0] and l1[0][1] == l2[1][1])
+                #s3 = (l1[0][0] == l2[0][0] and l1[0][1] == l2[0][1]) or (l1[1][0] == l2[1][0] and l1[1][1] == l2[1][1])
+
+    #returns the point on the cornor between l1 and l2, otherwise None if no cornor
+    def get_same_point(self, l1, l2):
+        if (l1 == None or l2 == None): return None
+        if (l1[0] == l2[0] or l1[0] == l2[1]): return l1[0]
+        if (l1[1] == l2[0] or l1[1] == l2[1]): return l1[1]
+        return None
+
 
     #returns laser_array
     def get_laser_array(self):
@@ -124,7 +156,6 @@ class Laser_Data:
             angle = math.radians(360) - abs(i*step - corr)
         return angle
 
-
     def get_fix_value(self):
         i = 0
         while self.get_true_angle(i) > math.radians(180):
@@ -156,6 +187,72 @@ class Laser_Data:
     def subtract(self, v1, v2):
         if (v1 == None or v2 == None): return None
         return (v1[0] - v2[0], v1[1] - v2[1])
+
+    #returns the dot-product of v1 and v2
+    def dot(self, v1, v2):
+        if (v1 == None or v2 == None): return None
+        return v1[0]*v2[0] + v1[1]*v2[1]
+
+    #returns the vector that is between point p1 and p2
+    def line_to_vector(self, l):
+        if (l == None): return None
+        p1 = l[0]
+        p2 = l[1]
+        if (p1 == None or p2 == None): return None
+        return (p2[0] - p1[0], p2[1] - p1[1])
+
+    #returns the point that has been rotated around center angle degrees.
+    def rotate_point(self, center, p, angle):
+        s = math.sin(angle)
+        c = math.cos(angle)
+
+        px = p[0] - center[0]
+        py = p[1] - center[1]
+
+        xnew = px * c + py * s
+        ynew = px * s + py * c
+
+        return (xnew + center[0], ynew + center[1])
+
+    #sets the element at x,y in 2D-array to value
+    def set_block(self, m, x, y, value):
+        if (x >= 0 and y >= 0 and y < len(m) and x < len(m[0])):
+            m[y][x] = value
+
+    #returns the length of vector vec
+    def vector_length(self, vec):
+        if (vec == None): return None
+        vecx = vec[0]
+        vecy = vec[1]
+        return math.sqrt(vecx * vecx + vecy * vecy)
+
+    #returns the length of line l
+    def line_length(self, p1, p2):
+        return math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]))
+
+    #using bressenhams line drawing algorithm to draw a line from p1 to p2 in 2d-list m
+    def draw_line(self, m, p1, p2, value):
+        x0 = p1[0]
+        y0 = p1[1]
+        x1 = p2[0]
+        y1 = p2[1]
+
+        if (x1 - x1 != 0):
+            m = (y1 * 1.0 - y0 * 1.0)/(x1 * 1.0 - x0 * 1.0)
+            y = y0
+            for x in range(x0, x1):
+                self.set_block(m, x, int(y + 0.5), value)
+                y += m
+        else:
+            for y in range(y0, y1):
+                self.set_block(m, x0, y, value)
+
+    #Returns the normalized vector
+    def normalize(self, vec):
+        if (vec == None): return None
+        length = self.vector_length(vec)
+        return (vec[0] / length, vec[1] / length)
+
 
     #returns the first delta
     def get_delta(self, i):
@@ -219,7 +316,7 @@ class Laser_Data:
         #I (JOHAN) HAVE NO IDEA WHAT THIS IS, BUT WE SHOULD FIX THIS
 
         self.lines = []
-
+        self.lines_score = []
         good_readings = []
         i = 0
         while i < self.size():
@@ -271,7 +368,6 @@ class Laser_Data:
 
     def set_intersections(self):
         self.intersections = []
-
         inter = []
 
         for v in self.vector_x:
@@ -313,7 +409,10 @@ class Laser_Data:
         all_lines = []
         closest_points = []
         line_score = []
-        #self.lines_sore 
+        #self.lines_sore
+
+        first = True
+
         for i in range(len(self.intersections)):
             for j in range(i, len(self.intersections), 1):
                 if (self.intersections[i] != self.intersections[j]):
@@ -330,8 +429,8 @@ class Laser_Data:
                     closest_2 = 0
                     hypot_a = None
                     hypot_b = None
-                    up = False
-                    num = 0
+
+
                     for k in range(self.size()):
                         pos = self.get_laser_pos(k)
                         x0 = pos[0]
@@ -343,6 +442,10 @@ class Laser_Data:
                         hypot_1 = math.sqrt(d_x1*d_x1 + d_y1*d_y1)
                         hypot_2 = math.sqrt(d_x2*d_x2 + d_y2*d_y2)
                         dist = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1) / math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1))
+
+
+
+
                         if hypot_a == None or hypot_1 < hypot_a:
                             hypot_a = hypot_1
                             closest_1 = k
@@ -351,60 +454,28 @@ class Laser_Data:
                             closest_2 = k
                         if dist < 5:
                             score += 1
+
                     if (score > 10):
+
                         l = (p1, p2)
-                        fac = 1
-                        if (closest_1 < closest_2): fac = closest_2 - closest_1
+                        val = 0
+                        c1 = closest_1
+                        c2 = closest_2
+                        tf = first
+                        if(first):
+                            c1 = closest_1
+                            c2 = closest_2
 
-                        if (closest_2 < closest_1): fac = closest_1 - closest_2
-                        #print(fac)
-                        print(closest_1, closest_2, fac)
-                        self.lines_score.append((l, closest_1, closest_2, score/fac, 0))
-                        #print(closest_1, closest_2)
-
-
-        #line score contains (line, closest1(i), closest2(i), score, finalscore)
-        """
-        for i in range(len(self.lines_score)):
-
-            i0 = self.lines_score[i][0]
-            i1 = self.lines_score[i][1]
-            i2 = self.lines_score[i][2]
-            i3 = self.lines_score[i][3]
-            i4 = self.lines_score[i][4]
-            print(i1, i2)
-            print ()
-            if (i == len(self.lines_score)-1):
-                i4 = i3 / (self.size() - (i1 - i2))
-            elif (i1 != i2):
-                i4 = i3 /(i2 - i1)
-            print("bejs", i4)
-
-            if (i1 > i2):
-                self.lines_score[i] = (i0, i2, i1, i3, i4)
-            else:
-                self.lines_score[i] = (i0, i1, i2, i3,i4)
-        """
-
-        
-        """
-        0.8247422680412371
-        0.07317073170731707
-        0.06707317073170732
-        0.9381443298969072
-        0.8955223880597015
-        0.765625
-
-        """
-        for ls in self.lines_score:
-            l = ls[0]
-            print("hehah",ls[3])
-            if(ls[4] > 0.7):
-                c.create_line(l[0][0] + offsetX, l[0][1] + offsetY, l[1][0] + offsetX, l[1][1] + offsetY)
-        sys.exit(0)
-
-
-
+                            val = self.size() - (c2 - c1)
+                            first = False
+                        else:
+                            if (closest_1 > closest_2):
+                                c1 = closest_2
+                                c2 = closest_1
+                            val = c2 - c1
+                        #print("HEHE: ", c1, c2, score, val, i, j)
+                        if (val > 0):
+                            self.lines_score.append((l, c1, c2, score, val))
 
     def draw_lines(self):
         line_expansion = 300
@@ -431,6 +502,9 @@ class Laser_Data:
                 self.vector_y.append((x0, y0, x1, y1))
 
         self.set_intersections()
+        self.set_cornor()
+
+
 
 
     def draw_dots(self):
@@ -455,20 +529,117 @@ class Laser_Data:
 
             c.create_oval(x + offsetX - 2, y + offsetY - 2, x+offsetX +2, y + offsetY + 2, fill =color)
 
-class Slam:
+class Mapping:
 
     def __init__(self):
         #The laser_array contains data of laser readings. 
         self.laser = Laser_Data()
+        self.grid = []
+        self.init_grid()
+
+    def init_grid(self):
+        # Init grid
+        self.grid = []
+        for y in range(21):
+            inner = []
+            for x in range(21):
+                inner.append("0")
+            self.grid.append(inner)
+
+    #creates a grid from
+    def fit_grid(self):
+
+        b_index = 0
+        b_angle = 10000
+        laser = self.laser
+        corn = laser.cornor
+        center = (0,0)
+
+        for i in range(len(corn)):
+
+            i1 = corn[i][0]
+            i2 = corn[i][1]
+
+            l1 = laser.lines_score[i1][0]
+            l2 = laser.lines_score[i2][0]
+
+            v1 = laser.normalize(laser.line_to_vector(l1))
+            v2 = laser.normalize(laser.line_to_vector(l2))
+            ang = math.acos(laser.dot(v1,v2))
+            rotvalue = 90 - abs(math.degrees(ang))
+            if (rotvalue <= 7):
+                if (ang < b_angle):
+                    b_angle = ang
+                    b_index = i
+
+            print(b_index, math.degrees(b_angle))
+            best_line = (laser.lines_score[corn[b_index][0]][0], laser.lines_score[corn[b_index][1]][0])
+            print(best_line)
+            best_vector = (laser.line_to_vector(best_line[0]), laser.line_to_vector(best_line[1]))
+            best_norm_vector = (laser.normalize(best_vector[0]), laser.normalize(best_vector[1]))
+            angle = math.acos(laser.dot(v1,v2))
+            print(math.degrees(angle))
+
+            rotated_lines = []
+            rotated_vec = []
+            rotated_center = (0,0)
+
+            if (90 - abs(math.degrees(angle)) <= 7):
+                print("LET'S ROTATE POINTS")
+                print(best_norm_vector)
+                deg_trans = math.atan(best_norm_vector[1][1] / best_norm_vector[1][0])
+                print("deg", math.degrees(deg_trans))
+                center_point = laser.get_same_point(best_line[0], best_line[1])
+                if (center_point != None):
+                    for i in range(len(laser.lines_score)):
+                        p1 = laser.lines_score[i][0][0]
+                        p2 = laser.lines_score[i][0][1]
+                        rp1 = laser.rotate_point(center_point, p1, -deg_trans)
+                        rp2 = laser.rotate_point(center_point, p2, -deg_trans)
+                        l = (rp1, rp2)
+                        rotated_lines.append(l)
+
+                        rotated_vec.append((rp2[0] - rp1[0], -1*(rp2[1] - rp1[1])))
+                    rotated_center = laser.rotate_point(center_point, center, -deg_trans)
+
+            print(rotated_center)
+            c2.create_oval(center_point[0] - 4 + offsetX, center_point[1] - 4 + offsetY, center_point[0]+4 + offsetX, center_point[1]+4 + offsetY)
+
+
+            for i in range(len(rotated_lines)):
+                l = rotated_lines[i]
+                if (laser.lines_score[i][3]/laser.lines_score[i][4] > 0.5):
+                    c2.create_line(l[0][0] + offsetX, l[0][1] + offsetY, l[1][0] + offsetX, l[1][1] + offsetY)
+
+
+
+
+
+
+
+
+
+
+            sys.exit(0)
+
+
+
+
+
+
+    def print_grid(self):
+        for g in self.grid:
+            print(g)
    
 
-s = Slam()
+s = Mapping()
 s.laser.update_laser(read_list[1])
-las = 0
+las = 1
 def draw():
     global s
     global las
     s.laser.update_laser(read_list[las])
+    #las += 1
     if (las >= len(read_list)):
         las = 0
     #print(s.laser.get_fix_value())
@@ -479,11 +650,30 @@ def draw():
     #print("oh yeah")
     #sys.exit(0)
     clearCanvas(c)
+    clearCanvas(c2)
 
     s.laser.draw_dots()
     s.laser.draw_lines()
+    """
+    for ls in s.laser.lines_score:
+        l = ls[0]
+        # print("hehah",ls[3])
+        thickness = ls[3] * 100 // 10
+        thickness = 4
+        val = ls[3] / ls[4]
+        dx = offsetX + (l[0][0] + l[1][0]) / 2
+        dy = offsetY + (l[0][1] + l[1][1]) / 2
+        t = "" + str(ls[1]) + ", " + str(ls[2]) + ", " + str(ls[3]) + ", " + str(ls[4])
+        t2 = "" + str(round(val, 3))
+        if (val > 0.5):
+            c2.create_line(l[0][0] + offsetX, l[0][1] + offsetY, l[1][0] + offsetX, l[1][1] + offsetY, width=thickness)
+        #if (val > 0.5):
+            c2.create_text(dx, dy, text = t,fill = "green")
+            c2.create_text(dx, dy+10, text=t2, fill="green")
+    #print(las)
+    """
+    s.fit_grid()
 
-
-    root.after(10, draw)
+    root.after(20, draw)
 root.after(1, draw)
 root.mainloop()

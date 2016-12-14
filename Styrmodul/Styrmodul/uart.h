03bd42301c36d76ec8e9e0fecd5ca4b02b8aebdd
Fredrik Iselius <freis685@student.liu.se>
2016-12-14 14:34:09 +0100

    Added Styrmodul project

---
+++ Styrmodul/Styrmodul/uart.h
@@ -0,0 +1,18 @@
+﻿/*
+ * uart.h
+ *
+ *  Updated: 14/12/2016
+ *  Authors: Fredrik Iselius
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+#define F_CPU 14745600
+
+void uart_init();
+void uart_send();
+void uart_receive();
+
+#endif /* UART_H_ */

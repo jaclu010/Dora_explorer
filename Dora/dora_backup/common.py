5be2f1be0f4eed81dfb0834b3a53172431db345a
marlu819 <marlu819@student.liu.se>
2016-12-21 11:55:06 +0100

    Cleaned up Dora dir

--- Dora/dora_backup/common.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def find_usb(port_id):
-	port = ""
-        for file in os.listdir("/dev/serial/by-id"):
-		if port_id in file:
-			symlink = os.path.join("/dev/serial/by-id/", file)
-			port = str(os.path.realpath(symlink))
-	return port
